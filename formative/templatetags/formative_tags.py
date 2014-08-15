10af87373fa179717dc364de35666c38c6b49c82
Jaap Roes <jaap@eight.nl>
2014-08-15 14:12:18 +0200

    Created a simple assignment tag to get formative blob data into templates

---
+++ formative/templatetags/formative_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from formative.models import FormativeBlob
+
+register = template.Library()
+
+
+@register.assignment_tag
+def get_formative_blob(unique_identifier):
+    """
+    Get a formative blob by its unique identifier and assign it to a
+    template variable.
+    """
+    try:
+        return FormativeBlob.objects.get(unique_identifier=unique_identifier)
+    except FormativeBlob.DoesNotExist:
+        return None
---
+++ tests/test_templatetags.py
@@ -0,0 +1,21 @@
+from django.template import Template, Context
+from django.test import TestCase
+from formative import registry
+
+
+class TestFormativeTags(TestCase):
+    def setUp(self):
+        SimpleForm = registry.get('simple').form
+        f = SimpleForm({
+            'unique_identifier': 'test-identifier',
+            'name': 'test-name'
+        })
+        f.full_clean()
+        self.obj = f.save()
+
+    def test_get_formative_blob(self):
+        t = Template('''{% load formative_tags %}
+        {% get_formative_blob 'test-identifier' as simple %}
+        {{ simple.unique_identifier }} - {{ simple.data.name }}''')
+        self.assertEqual(t.render(Context()).strip(),
+                         'test-identifier - test-name')
